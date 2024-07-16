6a8cf56d5bd117d3747d1c1068eb26b69d3abb11
Vivek Joshi <vivekjoshi210111242@gmail.com>
2024-07-16 10:57:57 +0530

    page config, title, and seclectbox added

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ import plotly.express as px # type: ignore
 
 df =  pd.pd.read_csv("bank.csv")
 
+st.set_page_config( 
+    page_title='Real time Data Science Dashboard',
+    page_icon='Red',
+    layout="wide"
+)
 
+st.title('Real-Time/Live Data Science Dashboard')
+
+job_filter = st.selectbox("Select the Job",pd.unique(df['job']))
 
