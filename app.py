dc35d7d24da61a73364cc5404f03b2a7d0bf8121
Vivek Joshi <vivekjoshi210111242@gmail.com>
2024-07-16 10:49:59 +0530

    CSV file added

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st # type: ignore
+import numpy as np
+import pandas as pd
+import time #to simulate a real time data 
+import plotly.express as px # type: ignore
+
+df =  pd.pd.read_csv("bank.csv")
+
+
+
